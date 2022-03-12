fbacc974c19a75031e9b2b30b76f4a9daeb123d0
Faishal Rizal <72137450+muhfaishalrizal@users.noreply.github.com>
2022-03-12 21:21:14 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 
-$hostDB = '127.0.0.1';
+$hostDB = 'ec2-3-230-238-86.compute-1.amazonaws.com';
 $portDB = '5432';
-$nameDB = 'mahasiswa';
-$userDB = 'postgres';
-$pwDB = '12345';
+$nameDB = 'd54g8r1vhrh8v8';
+$userDB = 'lwalqvxkfivigu';
+$pwDB = '1f5098befd9e84749e22537c4153aae6df18a313e0a5d2dce94625eebdc2ac2d';
 
 $connection = pg_connect("host=$hostDB port=$portDB dbname=$nameDB user=$userDB password=$pwDB");
 
